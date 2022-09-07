129d28f0574016772276c8baac11db678a4baf81
SyedAbdulhaq <syed.abdulhaq100@yahoo.com>
2022-09-07 13:18:23 -0700

    completed

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,13 +2,16 @@ import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext); // alter for passing props
+  const { feedback, isLoading } = useContext(FeedbackContext); // alter for passing props
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback</p>;
   }
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
@@ -24,6 +27,7 @@ function FeedbackList() {
       </AnimatePresence>
     </div>
   );
+
   // return (
   //   <div className="feedback-list">
   //     {feedback.map((item) => (
